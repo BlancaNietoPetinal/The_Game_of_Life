52b57027b153757c83519507a76c8b7294b332ea
Blanca Nieto Petinal <blancanietopetinal@gmail.com>
2023-02-13 11:47:42 +0100

    Docstrings added

--- gameoflifelib.py
+++ gameoflifelib.py
@@ -1,5 +1,6 @@
 """ Module for The Game of Life program"""
 import numpy as np
+
 def get_neig_mat(mat):
     """Returns the matrix of neighbours."""
     neigmat = np.zeros(mat.shape)
@@ -11,83 +12,88 @@ def get_neig_mat(mat):
 
 def neig_sum(row, col, mat):
     """Returns the neighbours of a given cell"""
-    sum = 0
+    summation = 0
     xlen=mat.shape[0]-1
     ylen=mat.shape[1]-1
     if in_bounds(row, col, mat.shape):
-        sum = inner_neig_sum(row,col,mat)
+        summation = inner_neig_sum(row,col,mat)
     elif(row==0 and col==0):
-        sum = left_upper_corner_sum(row,col,mat)
+        summation = left_upper_corner_neig_sum(row,col,mat)
     elif(row==0 and col<ylen):
-        sum = upper_bound_sum(row,col,mat)
+        summation = upper_bound_neig_sum(row,col,mat)
     elif(row==0 and col==ylen):
-        sum = right_upper_corner_sum(row,col,mat)
-    elif(row<xlen and col==0):  
-        sum = left_bound_sum(row,col,mat)    
+        summation = right_upper_corner_neig_sum(row,col,mat)
+    elif(row<xlen and col==0):
+        summation = left_bound_neig_sum(row,col,mat)
     elif(row!=xlen and col==ylen):
-        sum = right_bound_sum(row,col, mat)
+        summation = right_bound_neig_sum(row,col, mat)
     elif(row==xlen and col==0):
-        sum = left_lower_corner_sum(row,col,mat)
+        summation = left_lower_corner_neig_sum(row,col,mat)
     elif (row==xlen and col<ylen):
-        sum = lower_border_sum(row,col,mat)
+        summation = lower_bound_neig_sum(row,col,mat)
     else:
-        sum = right_lower_corner(row,col,mat)
-    return sum
+        summation = right_lower_corner_neig_sum(row,col,mat)
+    return summation
 
-
-    return sum
-
-def right_lower_corner(row,col,mat):
+def right_lower_corner_neig_sum(row,col,mat):
+    """Sums the neighbours of the right lower corner"""
     top = mat[row-1,col]
     topleft = mat[row-1,col-1]
     left = mat[row,col-1]
-    return (top+topleft+left)
+    return top+topleft+left
 
-def lower_border_sum(row,col,mat):
+def lower_bound_neig_sum(row,col,mat):
+    """Sums the neighbours of the lower bound"""
     left = mat[row,col-1]
     topleft = mat[row-1,col-1]
     top = mat[row-1,col]
     topright = mat[row-1,col+1]
     right = mat[row,col+1]
-    return (top+topright+right+topleft+left)
+    return top+topright+right+topleft+left
 
-def left_lower_corner_sum(row,col,mat):
+def left_lower_corner_neig_sum(row,col,mat):
+    """Sums the neighbours of the left lower corner cell"""
     top = mat[row-1,col]
     topright = mat[row-1,col+1]
     right = mat[row,col+1]
-    return (top+topright+right)
+    return top+topright+right
 
-def right_bound_sum(row,col, mat):
+def right_bound_neig_sum(row,col, mat):
+    """Sums the neighbours of the right bound cells"""
     top = mat[row-1,col]
     topleft = mat[row-1,col-1]
     left = mat[row,col-1]
     bottomleft = mat[row+1,col-1]
     bottom = mat[row+1,col]
-    return(left+top+bottom+topleft+bottomleft)
+    return left+top+bottom+topleft+bottomleft
 
-def left_bound_sum(row,col,mat): 
-    top = mat[row-1,col] 
-    topright = mat[row-1,col+1]  
+def left_bound_neig_sum(row,col,mat):
+    """Sums the neighbours of the left bound cells"""
+    top = mat[row-1,col]
+    topright = mat[row-1,col+1]
     right = mat[row,col+1]
     bottomright = mat[row+1,col+1]
     bottom = mat[row+1,col]
-    return(right+top+bottom+bottomright+topright)
+    return right+top+bottom+bottomright+topright
 
-def right_upper_corner_sum(row,col,mat):
+def right_upper_corner_neig_sum(row,col,mat):
+    """Sums the neighbours of the right upper corner cell"""
     left = mat[row,col-1]
     bottom = mat[row+1,col]
     bottomleft = mat[row+1,col-1]
-    return (left+bottom+bottomleft)
+    return left+bottom+bottomleft
 
-def upper_bound_sum(row,col,mat):
+def upper_bound_neig_sum(row,col,mat):
+    """Sums the neighbours of the upper bound cells"""
     right = mat[row,col+1]
     left = mat[row,col-1]
     bottom = mat[row+1,col]
-    bottomright = mat[row+1,col+1]  
+    bottomright = mat[row+1,col+1]
     bottomleft = mat[row+1,col-1]
-    return(right+left+bottom+bottomright+bottomleft)
+    return right+left+bottom+bottomright+bottomleft
 
 def inner_neig_sum(row,col,mat):
+    """Sums the neighbours of the in bounds cells"""
     left = mat[row,col-1]
     right = mat[row,col+1]
     top = mat[row-1,col]
@@ -96,20 +102,21 @@ def inner_neig_sum(row,col,mat):
     topright = mat[row-1,col+1]
     bottomleft = mat[row+1,col-1]
     bottomright = mat[row+1,col+1]
-    return (left+right+top+bottom+topleft+topright+bottomleft+bottomright)
+    return left+right+top+bottom+topleft+topright+bottomleft+bottomright
 
-def left_upper_corner_sum(row,col,mat):
+def left_upper_corner_neig_sum(row,col,mat):
+    """Sums the neighbours of the left upper corner cell"""
     right = mat[row,col+1]
     bottom = mat[row+1,col]
     bottomright = mat[row+1,col+1]
-    return (right+bottom+bottomright)
+    return right+bottom+bottomright
 
 def in_bounds(row, col, matshape):
     """Checks if the cell is not a border of the grid"""
     xbound = matshape[0]-1
     ybound = matshape[1]-1
     if(row==0 or col==0) or (row==xbound or col==ybound):
-        bool = False
+        res = False
     else:
-        bool = True
-    return bool
+        res = True
+    return res
