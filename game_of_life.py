e72cea1ac9393735057989138c3ac6a2533946ce
Blanca Nieto Petinal <blancanietopetinal@gmail.com>
2023-02-15 10:31:17 +0100

    If statement fixed

--- game_of_life.py
+++ game_of_life.py
@@ -9,11 +9,11 @@ cellsgrid = np.zeros([GRID_SIZE, GRID_SIZE])
 nextcellsgrid = np.zeros([GRID_SIZE, GRID_SIZE])
 
 # Introduce cells
-cellsgrid[14,15] = 1
 cellsgrid[15,15] = 1
-cellsgrid[15,15] = 1
-cellsgrid[15,16] = 1
-cellsgrid[15,17] = 1
+cellsgrid[15,14] = 1
+cellsgrid[15,13] = 1
+cellsgrid[15,12] = 1
+
 
 
 plt.imshow(cellsgrid)
@@ -22,27 +22,14 @@ for i in range(N_ITER):
     neigmat = gol.get_neig_mat(cellsgrid)
     for row in range(GRID_SIZE):
         for col in range(GRID_SIZE):
-            if(cellsgrid[row,col]==1 and neigmat[row,col]<2):
+            if(cellsgrid[row,col]==0 and neigmat[row,col]==3):
+                nextcellsgrid[row,col]=1
+            elif(cellsgrid[row,col]==1 and (neigmat[row,col]<2 or neigmat[row,col]>3)):
                 nextcellsgrid[row,col]=0
-            elif(cellsgrid[row,col]==1):
+            elif(cellsgrid[row,col]==1 and (neigmat[row,col]==2 or neigmat[row,col]==3)):
                 nextcellsgrid[row,col]=1
-            elif(cellsgrid[row,col]==0 and neigmat[row,col]>=3):
-                nextcellsgrid[row,col]=1
+
     cellsgrid=nextcellsgrid
     plt.imshow(cellsgrid)
     plt.show()
-
-
-
-
-
-# plt.figure(1)
-# plt.subplot(121)
-# plt.imshow(neigmat)
-# plt.subplot(122)
-# plt.imshow(cellsgrid)
-# plt.show()
-
-
-
 
